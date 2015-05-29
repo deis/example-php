ab279133cac4e010ba1393a07e33f51084ef12cb
Davey Shafik <me@daveyshafik.com>
2015-05-29 17:02:49 -0400

    Display container ID

--- web/index.php
+++ web/index.php
@@ -4,7 +4,9 @@ require('../vendor/autoload.php');
 
 $app = new \Slim\Slim();
 $app->get('/', function () {
-    echo "Powered by " . ($_ENV["POWERED_BY"] ? $_ENV["POWERED_BY"] : "Deis") . "\n";
+    $container = trim(exec('hostname') ?: "unknown");
+    echo "Powered by " . (isset($_ENV["POWERED_BY"]) ? $_ENV['POWERED_BY'] : "Deis!") . "\n";
+    echo "Running on container ID " . $container . "\n";
 });
 $app->run();
 
