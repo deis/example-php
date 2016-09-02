345d412b4be5c3584e46364c4c7a6bbca72c0a12
Joshua Anderson <j@joshua-anderson.com>
2016-09-02 13:31:43 -0700

    feat(*): update php and output (#9)

--- web/index.php
+++ web/index.php
@@ -1,12 +1,19 @@
 <?php
-
 require('../vendor/autoload.php');
+use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Message\ResponseInterface;
 
-$app = new \Slim\Slim();
-$app->get('/', function () {
+$app = new Slim\App();
+
+// This endppint returns 200 for kubernetes healthchecks.
+$app->get('/healthz', function (ServerRequestInterface $request, ResponseInterface $response) {
+    return $response->withStatus(200);
+});
+$app->get('/', function (ServerRequestInterface $request, ResponseInterface $response) {
+    $powered = isset($_ENV["POWERED_BY"]) ? $_ENV['POWERED_BY'] : "Deis";
     $container = trim(exec('hostname') ?: "unknown");
-    echo "Powered by " . (isset($_ENV["POWERED_BY"]) ? $_ENV['POWERED_BY'] : "Deis!") . "\n";
-    echo "Running on container ID " . $container . "\n";
+    $release = isset($_ENV["WORKFLOW_RELEASE"]) ? $_ENV['WORKFLOW_RELEASE'] : "unknown";
+    return $response->withStatus(200)->write("Powered by $powered\nRelease $release on $container\n");
 });
 $app->run();
 
